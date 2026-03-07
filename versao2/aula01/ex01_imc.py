b739acec39826626b07abba5ae8ad45487b1ab49
MARCO ANTONIO MAZZA CANEDO DOS SANTOS <marcocanedo@ufpr.br>
2026-03-06 21:29:00 -0300

    Revert "Adiciona versao2 com estrutura para trabalho em equipe"
    This reverts commit 07716c3d87152f64c619cb2e0f0d1ff66fd6f331.

--- versao2/aula01/ex01_imc.py
+++
@@ -1,5 +0,0 @@
-"""Calcula o IMC a partir de peso e altura informados pelo usuário."""
-peso = float(input("Digite o peso (kg): "))
-altura = float(input("Digite a altura (m): "))
-imc = peso / (altura * altura)
-print(f"Seu IMC é: {imc:.2f}")
